2e6a4da444491085ccff31d59d1451cf054954ee
luisvfandrade <luis.vilhena@tecnico.ulisboa.pt>
2022-05-31 14:38:30 +0100

    :p
    Co-authored-by: Catarina Bento <catarinab@users.noreply.github.com>
    Co-authored-by: Afonso <Uhphonsoo@users.noreply.github.com>

--- Trabalho-2/js/trabalho.js
+++ Trabalho-2/js/trabalho.js
@@ -4,125 +4,95 @@ var perspCamera, orthoCamera, camera;
 
 var scene, renderer, material
 
-// Objects
-var torus1, dodecahedron1, icosahedron1, octahedron1, sphere1, box1;
-
 var clock, delta;
 
-// Object Scales
-// const ;
+// Objects
+var planet, dodecahedron1, icosahedron1, octahedron1, sphere1, box1;
+
+const scale = 1;
 
 const movingFactor = 5, rotationFactor = 1;
 
-const scale = 1.5;
+// Objects Scales
+const planetRadius = 36, rocketWingspan = planetRadius / 12, trashWingspan = planetRadius / 20, objectOrbit = planetRadius * 1.2;
 
-function createTorus(object) {
-    'use strict';
-
-    const radius = 0.45 * scale;
-    const tubeRadius = 0.2 * scale;
-    const radialSegments = 8;
-    const tubularSegments = 24;
-
-    material = new THREE.MeshBasicMaterial({ color: 0xedb381, wireframe: true });
-    let geometry = new THREE.TorusBufferGeometry(radius, tubeRadius, radialSegments, tubularSegments);
-    let mesh = new THREE.Mesh(geometry, material);
-    mesh.position.set(0 * scale, 0 * scale, 0 * scale);    
-
-    object.add(mesh);
-    // object.position.set(0 * scale, 0 * scale, 0 * scale);
+function sphericalSet(object, radius, theta, phi) {
+    let x = radius * Math.cos(theta) * Math.sin(phi);
+    let z = radius * Math.sin(theta) * Math.sin(phi);
+    let y = radius * Math.cos(phi);
+    
+    object.position.set(x, y, z);
 }
-
-/* 
-function createCapsule(object) {
-    'use strict';
-
-    material = new THREE.MeshBasicMaterial({ color: 0xedb381, wireframe: true });
-    let geometry = new THREE.CapsuleGeometry( 1, 1, 4, 8 );
-    let mesh = new THREE.Mesh(geometry, material);
-
-    mesh.position.set(0 * scale, 10 * scale, 0 * scale);    
-
-    object.add(mesh);
-    // object.position.set(0 * scale, 0 * scale, 0 * scale);
-} */
 
 function createDodecahedron(object) {
     'use strict';
 
-    const radius = 0.45 * scale;
-
     material = new THREE.MeshBasicMaterial({ color: 0xedb381, wireframe: true });
-    let geometry = new THREE.DodecahedronBufferGeometry(radius);
+    let geometry = new THREE.DodecahedronBufferGeometry((trashWingspan / 2) * scale);
     let mesh = new THREE.Mesh(geometry, material);
     mesh.position.set(0 * scale, 2 * scale, 0 * scale);    
 
     object.add(mesh);
-    // object.position.set(0 * scale, 2 * scale, 0 * scale);
 }
 
 function createIcosahedron(object) {
     'use strict';
 
-    const radius = 0.45 * scale;
-
     material = new THREE.MeshBasicMaterial({ color: 0xedb381, wireframe: true });
-    let geometry = new THREE.IcosahedronBufferGeometry(radius);
+    let geometry = new THREE.IcosahedronBufferGeometry((trashWingspan / 2) * scale);
     let mesh = new THREE.Mesh(geometry, material);
 
     mesh.position.set(0 * scale, 4 * scale, 0 * scale);    
 
     object.add(mesh);
-    // object.position.set(0 * scale, 0 * scale, 0 * scale);
 }
 
 function createOctahedron(object) {
     'use strict';
 
-    const radius = 0.45 * scale;
-
     material = new THREE.MeshBasicMaterial({ color: 0xedb381, wireframe: true });
-    let geometry = new THREE.OctahedronBufferGeometry(radius);
+    let geometry = new THREE.OctahedronBufferGeometry((trashWingspan / 2) * scale);
     let mesh = new THREE.Mesh(geometry, material);
 
     mesh.position.set(0 * scale, 6 * scale, 0 * scale);    
 
     object.add(mesh);
-    // object.position.set(0 * scale, 0 * scale, 0 * scale);
 }
 
 function createSphere(object) {
     'use strict';
 
-    const radius = 0.45 * scale;
-    const widthSegments = 12;
-    const heightSegments = 8;
-
     material = new THREE.MeshBasicMaterial({ color: 0xedb381, wireframe: true });
-    let geometry = new THREE.SphereBufferGeometry(radius, widthSegments, heightSegments);
+    let geometry = new THREE.SphereBufferGeometry((trashWingspan / 2) * scale, 10 * scale, 10 * scale);
     let mesh = new THREE.Mesh(geometry, material);
 
     mesh.position.set(0 * scale, 8 * scale, 0 * scale);    
 
     object.add(mesh);
-    // object.position.set(0 * scale, 0 * scale, 0 * scale);
 }
 
 function createBox(object) {
     'use strict';
 
-    const width = 0.9 * scale;
-    const height = 0.9 * scale;
-    const depth = 0.9 * scale;
-
     material = new THREE.MeshBasicMaterial({ color: 0xedb381, wireframe: true });
-    let geometry = new THREE.BoxBufferGeometry(width, height, depth);
+    let geometry = new THREE.BoxBufferGeometry(trashWingspan * scale, trashWingspan * scale, trashWingspan * scale);
     let mesh = new THREE.Mesh(geometry, material);
 
     mesh.position.set(0 * scale, 10 * scale, 0 * scale);    
 
     object.add(mesh);
-    // object.position.set(0 * scale, 0 * scale, 0 * scale);
+}
+
+function createPlanet(object) {
+    'use strict';
+
+    material = new THREE.MeshBasicMaterial({ color: 0xedb381, wireframe: true });
+    let geometry = new THREE.SphereGeometry(planetRadius * scale, 30 * scale, 30 * scale);
+    let mesh = new THREE.Mesh(geometry, material);
+
+    mesh.position.set(0 * scale, 10 * scale, 0 * scale);    
+
+    object.add(mesh);
 }
 
 function createCameras() {
@@ -147,35 +117,29 @@ function createScene() {
 
     scene = new THREE.Scene();
 
-    scene.add(new THREE.AxisHelper(10));
+    scene.add(new THREE.AxesHelper(10));
 
-    torus1 = new THREE.Object3D();
-    // torus1.userData = {rotating: false, incrementFactor: rotationFactor};
-    createTorus(torus1);
-    scene.add(torus1);
+    planet = new THREE.Object3D();
+    createPlanet(planet);
+    scene.add(planet);
 
     dodecahedron1 = new THREE.Object3D();
-    // dodecahedron1.userData = {rotating: false, incrementFactor: rotationFactor};
     createDodecahedron(dodecahedron1);
     scene.add(dodecahedron1);
 
     icosahedron1 = new THREE.Object3D();
-    // icosahedron1.userData = {rotating: false, incrementFactor: rotationFactor};
     createIcosahedron(icosahedron1);
     scene.add(icosahedron1);
 
     octahedron1 = new THREE.Object3D();
-    // octahedron1.userData = {rotating: false, incrementFactor: rotationFactor};
     createOctahedron(octahedron1);
     scene.add(octahedron1);
 
     sphere1 = new THREE.Object3D();
-    // sphere1.userData = {rotating: false, incrementFactor: rotationFactor};
     createSphere(sphere1);
     scene.add(sphere1);
 
     box1 = new THREE.Object3D();
-    // box1.userData = {rotating: false, incrementFactor: rotationFactor};
     createBox(box1);
     scene.add(box1);
 }
@@ -204,7 +168,6 @@ function onKeyDown(e) {
     'use strict';
 
     switch(e.keyCode) {
-        // deixado para referencia
         case 49: //1, Orthographic Camera
             camera = orthoCamera;
             camera.position.x = 30 * scale;
@@ -228,26 +191,12 @@ function onKeyDown(e) {
             break;
         
         case 39: // right arrow, Move Rocket Longitudinally
-            // Reference
-            /* if (foundation.userData.movingX == false) {
-                foundation.userData.movingX = true;
-                foundation.userData.incrementFactorX = movingFactor;
-            } */
             break;
         case 37: // left arrow, Move Rocket Longitudinally
-            /* if () {
-                ;
-            } */
             break;
         case 38: //up arrow, Move Rocket Latitudinally
-            /* if () {
-                ;
-            } */
             break;
         case 40: //down arrow, Move Rocket Latitudinally
-            /* if () {
-                ;
-            } */
             break;
     }
 }
@@ -259,17 +208,9 @@ function onKeyUp(e) {
 
         case 39: // right arrow
         case 37: // left arrow
-            // foundation.userData.movingX = false;
             break;
         case 38: //up arrow
         case 40: //down arrow
-            // foundation.userData.movingY  = false;
-            break;
-        case 68: //D
-        case 100: //d
-        case 67: //C
-        case 99: //c
-            // foundation.userData.movingZ  = false;
             break;
     }
 }
@@ -283,17 +224,6 @@ function animate() {
     'use strict';
 
     delta = clock.getDelta();
-
-    // deixado para referencia
-    /* if (pivot.userData.rotating) {
-        let step = pivot.userData.incrementFactor * delta;
-        pivot.rotateX(step);
-    } */
-
-    /* if (foundation.userData.movingX) {
-        let step = foundation.userData.incrementFactorX * delta;
-        foundation.translateX(step);
-    } */
 
     render();
 
