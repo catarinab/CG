e6e41891e4b829046e5e60694f2eb7cdbd8c979b
Uhphonsoo <joao.afonso.hs@gmail.com>
2022-06-13 20:45:05 +0100

    Many changes

--- Trabalho-3/js/trabalho.js
+++ Trabalho-3/js/trabalho.js
@@ -1,67 +1,64 @@
 /* global THREE */
 
-var orthoCam, perspCam, perspCam, camera, ambientLight, dirLight;
+var orthoCam, perspCam, vrCam, camera, dirLight, spotLightZ, spotLightX, spotLightC, ambientLight;
 
 var scene, renderer;
 
 var clock, delta;
 
 // Objects
-var floor, podium, smallStep, bigStep;
+var floor, podium, steps;
 
 // Constants
-const scale = 2, perspCamY = 11;
+const scale = 1, perspCamY = 11;
 
 // Objects Scales
-const floorWidth = 14, floorHeight = 14, podiumWidth = 7, podiumHeight = 2, podiumDepth = 7,
-        stepWidth = 1, stepHeight = 1, stepDepth = 0.5;
+const floorWidth = 14, floorLength = 14, podiumWidth = 7, podiumHeight = 2, podiumDepth = 7,
+        stepWidth = 1, stepDepth = 0.5;
 
 function createLights() {
     'use strict';
 
+    // Directional Light
     dirLight = new THREE.DirectionalLight(0xffffff);
     dirLight.position.set(0, perspCamY, 0);
     dirLight.target.position.set(0, 0, -5);
+    scene.add(dirLight);
+
+    // Spot Lights
 
     ambientLight = new THREE.AmbientLight(0xffffff);
-
-    scene.add(dirLight);
-    //scene.add(ambientLight);
+    scene.add(ambientLight);
 }
 
 function createSteps() {
     'use strict';
 
-    smallStep = new THREE.Object3D();
-    bigStep = new THREE.Object3D();
+    steps = new THREE.Object3D();
     
     // Gouraud Shading
-    let lambertMaterial = new THREE.MeshLambertMaterial({ color: 0xffffff, wireframe: false });
-    let lambertMaterial2 = new THREE.MeshLambertMaterial({ color: 0xfff34f, wireframe: false });
+    let lambertMaterialBig = new THREE.MeshLambertMaterial({ color: 0x55342b, wireframe: false });
+    let lambertMaterialSmall = new THREE.MeshLambertMaterial({ color: 0x55342b, wireframe: false });
     // Phong Shading
-    let phongMaterial = new THREE.MeshPhongMaterial({ color: 0xffffff, wireframe: false });
+    let phongMaterialBig = new THREE.MeshPhongMaterial({ color: 0x55342b, wireframe: false });
+    let phongMaterialSmall = new THREE.MeshPhongMaterial({ color: 0x55342b, wireframe: false });
 
-    let smallStepGeometry = new THREE.BoxGeometry(stepWidth * scale, stepHeight * scale, stepDepth * scale);
-    let smallStepMesh = new THREE.Mesh(smallStepGeometry, lambertMaterial2); 
-    smallStepMesh.position.set(0, 0, 0);
-
-    let bigStepGeometry = new THREE.BoxGeometry(stepWidth * scale, stepHeight * 2 * scale, stepDepth * scale);
-    let bigStepMesh = new THREE.Mesh(bigStepGeometry, lambertMaterial); 
+    let bigStepGeometry = new THREE.BoxGeometry(stepWidth * scale, (2 * podiumHeight / 3) * scale, stepDepth * scale);
+    let bigStepMesh = new THREE.Mesh(bigStepGeometry, lambertMaterialBig); 
     bigStepMesh.position.set(0, 0, 0);
-
-    //podium.translateY(podiumHeight/2 * scale);
-
-    smallStep.translateY(stepHeight/2 * scale);
-    bigStep.translateY(stepHeight * scale);
-
-    smallStep.translateZ(podiumDepth + stepDepth * 3);
-    bigStep.translateZ(podiumDepth + stepDepth);
     
-    smallStep.add(smallStepMesh); 
-    bigStep.add(bigStepMesh); 
+    let smallStepGeometry = new THREE.BoxGeometry(stepWidth * scale, (podiumHeight / 3) * scale, stepDepth * scale);
+    let smallStepMesh = new THREE.Mesh(smallStepGeometry, lambertMaterialSmall); 
+    smallStepMesh.position.set(0, - (podiumHeight / 6) * scale, stepDepth * scale);
 
-    scene.add(smallStep);
-    scene.add(bigStep);
+    steps.userData = {altMaterial: [phongMaterialBig, phongMaterialSmall], meshes: [bigStepMesh, smallStepMesh]};
+
+    steps.add(smallStepMesh); 
+    steps.add(bigStepMesh);
+    steps.translateY((podiumHeight / 3) * scale);
+    steps.translateZ((podiumDepth / 2 + stepDepth / 2) * scale);
+
+    scene.add(steps);
 }
 
 function createPodium() {
@@ -70,17 +67,19 @@ function createPodium() {
     podium = new THREE.Object3D();
     
     // Gouraud Shading
-    let lambertMaterial = new THREE.MeshLambertMaterial({ color: 0x404040, wireframe: false });
+    let lambertMaterial = new THREE.MeshLambertMaterial({ color: 0x55342b, wireframe: false });
     // Phong Shading
-    let phongMaterial = new THREE.MeshPhongMaterial({ color: 0x4fd0e7, wireframe: false });
+    let phongMaterial = new THREE.MeshPhongMaterial({ color: 0x55342b, wireframe: false });
 
     let geometry = new THREE.BoxGeometry(podiumWidth * scale, podiumHeight * scale, podiumDepth * scale);
     let mesh = new THREE.Mesh(geometry, lambertMaterial); 
     mesh.position.set(0, 0, 0);
 
-    podium.translateY(podiumHeight/2 * scale);
+    podium.userData = {altMaterial: phongMaterial};
     
     podium.add(mesh);  
+    podium.translateY(podiumHeight/2 * scale);
+
     scene.add(podium);
 }
 
@@ -90,17 +89,19 @@ function createFloor() {
     floor = new THREE.Object3D();
     
     // Gouraud Shading
-    let lambertMaterial = new THREE.MeshLambertMaterial({ color: 0x4fd0e7, wireframe: false });
+    let lambertMaterial = new THREE.MeshLambertMaterial({ color: 0x711324, wireframe: false });
     // Phong Shading
-    let phongMaterial = new THREE.MeshPhongMaterial({ color: 0x4fd0e7, wireframe: false });
+    let phongMaterial = new THREE.MeshPhongMaterial({ color: 0x711324, wireframe: false });
 
-    let geometry = new THREE.PlaneGeometry(floorWidth * scale, floorHeight * scale);
+    let geometry = new THREE.PlaneGeometry(floorWidth * scale, floorLength * scale);
     let mesh = new THREE.Mesh(geometry, lambertMaterial); 
     mesh.position.set(0, 0, 0);
 
-    floor.rotateX(-Math.PI/2);
+    floor.userData = {altMaterial: phongMaterial};
     
-    floor.add(mesh);  
+    floor.add(mesh);
+    floor.rotateX(-Math.PI/2);
+
     scene.add(floor);
 }
 
@@ -109,9 +110,9 @@ function createCameras() {
     
     perspCam = new THREE.PerspectiveCamera(70, 
         window.innerWidth / window.innerHeight, 1, 100);
-    perspCam.position.x = 0;
-    perspCam.position.y = perspCamY * scale;
-    perspCam.position.z = 0 * scale;
+    perspCam.position.x = -10 * scale;
+    perspCam.position.y = 15 * scale;
+    perspCam.position.z = 10 * scale;
     perspCam.lookAt(scene.position);
     
     orthoCam = new THREE.OrthographicCamera(window.innerWidth / - 2, 
@@ -119,14 +120,12 @@ function createCameras() {
     orthoCam.zoom = 40 / scale;
     orthoCam.updateProjectionMatrix();
     orthoCam.position.x = 0;
-    orthoCam.position.y = perspCamY * scale;
-    orthoCam.position.z = 0 * scale;
-    /* orthoCam.position.x = 50 * scale;
     orthoCam.position.y = 0;
-    orthoCam.position.z = 0; */
+    orthoCam.position.z = (floorWidth * 5) * scale;
     orthoCam.lookAt(scene.position);
+    orthoCam.position.y = podiumHeight * scale;
 
-    camera = orthoCam;
+    camera = perspCam;
 }
 
 function createScene() {
@@ -146,11 +145,13 @@ function onResize() {
     renderer.setSize(window.innerWidth, window.innerHeight);
 
     if (window.innerHeight > 0 && window.innerWidth > 0) {
-        if (camera == fixedPerspCam || camera == perspCam) {
+        if (camera == perspCam) {
+            console.log("hey puta\n");
             camera.aspect = renderer.getSize().width / renderer.getSize().height;
             camera.updateProjectionMatrix();
         }
         else {
+            console.log("wtf\n");
             camera.left = window.innerWidth / -2;
             camera.right = window.innerWidth / 2;
             camera.bottom = window.innerHeight / -2;
